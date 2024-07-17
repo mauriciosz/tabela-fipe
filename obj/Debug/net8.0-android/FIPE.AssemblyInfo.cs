00e3404166a7e0549bd7d72360e6d0bb197d5bf8
Mauricio Zaccaro <neto.mauricio@unemat.br>
2024-07-16 20:45:00 -0400

    Aplicativo Tabela FIPE em .NET MAUI concluído na versão 1.0.0

--- obj/Debug/net8.0-android/FIPE.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/FIPE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FIPE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6e6a055ec6a97710d6a64d3e8be3d2172a15b2f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FIPE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FIPE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("FIPE.MainApplication, FIPE, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc649b8a0d7e760f5703.MainApplication.class, crc649b8a0d7e760f5703.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
